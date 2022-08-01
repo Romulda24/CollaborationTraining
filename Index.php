8b254cbe80e867d0bbd1f8aa4322912adb342d1f
Sangadji999 <Riamariksangadji@gmail.com>
2022-08-01 23:48:33 +0700

    ada perubahan file index.php

--- Index.php
+++ Index.php
@@ -2,6 +2,5 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3>Romulda Ririndo Westi(195610042)</h3>"
-echo "<h3>Romulda Ririndo Westi(195610042)</h3>"
 
 ?>
