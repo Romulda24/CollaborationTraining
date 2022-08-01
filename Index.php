91a3f00db3ec22bcd718d09752eb6bdf443fcc79
nadirarmlnii <nadira.ramlani@students.utdi.ac.id>
2022-08-01 23:26:42 +0700

    rubah file index.php

--- Index.php
+++ Index.php
@@ -2,5 +2,4 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3>Romulda Ririndo Westi(195610042)</h3>"
-echo "<h3>Praktek</h3>"
 ?>
